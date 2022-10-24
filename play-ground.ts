5ec4f33e7225bb97394867cd8806469979faccdf
jmnghn <jeongmyeonghyeon@gmail.com>
2022-10-25 00:11:44 +0900

    Update Notes

--- play-ground.ts
+++ play-ground.ts
@@ -1,9 +1,9 @@
-class A {
-  readonly a: string;
-  b: string;
+function add<T extends number | string>(x: T, y: T): T {
+  return x + y;
 }
-
-class B implements A {
-  a: string = "123";
-  b: string = "world";
-}
+add<number>(1, 2);
+add(1, 2);
+add<string>("1", "2");
+add("1", "2");
+add(1, "2");
+add(true, false);
