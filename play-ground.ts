fc7eb9712c47dd27fdc799e1952e23dc569c483e
jmnghn <jeongmyeonghyeon@gmail.com>
2022-10-29 21:29:15 +0900

    Update Notes

--- play-ground.ts
+++ play-ground.ts
@@ -1,180 +1,187 @@
-// // 1 - ### `Parameters<Type>`
-// // 함수 유형 Type의 '매개변수에 사용된 타입'에서 튜플 타입을 생성합니다.
+// Promise는 Promise<결과값>타입으로 표시합니다.
 
-// // #### 공식문서 Example 코드
-// declare function f1(arg: { a: number; b: string }): void;
+// // 1 - ### Promise.all
+// 다음은 3개의 Promise를 받아 결과값을 리턴하는 코드다.
+// 그 중 p1이 좀 특이하다.
+// 바로 성공하는 Promise인데, 1을 resolve하고 then이 연달아 3번 붙어 있고, 마지막 then에서는 문자열을 반환하는 형태다.
+// 이런 형태를 Promise.all에 넣었을 때 결과(result)가 어떻게 되냐면
+// 위 then을 모두 실행하고 최종 결과값을 result에서 return한다.
 
-// // type T0 = [];
-// type T0 = Parameters<() => string>;
+// const p1 = Promise.resolve(1)
+//   .then((a) => a + 1)
+//   .then((a) => a + 1)
+//   .then((a) => a.toString());
+// const p2 = Promise.resolve(2);
+// const p3 = new Promise((res, rej) => {
+//   setTimeout(res, 1000);
+// });
 
-// // type T1 = [s: string];
-// type T1 = Parameters<(s: string) => void>;
+// // [타입추론] (parameter) result: [string, number, unknown] // 😳🤔 타입스크립트는 어떻게 p1의 변화하는 타입을 알 수 있었을까?
+// Promise.all([p1, p2, p3]).then((result) => {
+//   console.log(result); // [ '3', 2, undefined ]
+// });
 
-// // type T2 = [arg: unknown];
-// type T2 = Parameters<<T>(arg: T) => T>;
+// // 2 - #### Promise.all을 살펴보자
 
-// // type T3 = [
-// //   arg: {
-// //     a: number;
-// //     b: string;
-// //   }
-// // ];
-// type T3 = Parameters<typeof f1>;
+// const p1 = Promise.resolve(1)
+//   .then((a) => a + 1)
+//   .then((a) => a + 1)
+//   .then((a) => a.toString());
+// const p2 = Promise.resolve(2);
+// const p3 = new Promise((res, rej) => {
+//   setTimeout(res, 1000);
+// });
 
-// // type T4 = unknown[];
-// type T4 = Parameters<any>;
+// Promise.all([p1, p2, p3]).then((result) => {
+//   console.log(result); // [ '3', 2, undefined ]
+// });
 
-// // type T5 = never;
-// type T5 = Parameters<never>;
-
-// // type T6 = never
-// type T6 = Parameters<string>; // ❌ Type 'string' does not satisfy the constraint '(...args: any) => any'.
-
-// // type T7 = never
-// type T7 = Parameters<Function>;
-// // ❌ Type 'Function' does not satisfy the constraint '(...args: any) => any'.
-// // Type 'Function' provides no match for the signature '(...args: any): any'.
-
-// 2 - 다른 Example 코드
-// function zip(
-//   x: number,
-//   y: string,
-//   z: boolean
-// ): { x: number; y: string; z: boolean } {
-//   return { x, y, z };
+// // lib.es2015.promise.d.ts
+// // 😳
+// interface PromiseConstructor {
+//   all<T extends readonly unknown[] | []>(
+//     values: T
+//   ): Promise<{ -readonly [P in keyof T]: Awaited<T[P]> }>;
 // }
 
-// // type Params = Parameters<zip>; // ❌ 'zip' refers to a value, but is being used as a type here. Did you mean 'typeof zip'?ts(2749)
-
-// // 함수를 바로 타입으로 사용할수는 없기 때문에 typeof를 붙여줘야 한다.
-// // type Params = [x: number, y: string, z: boolean] // 튜플 형태
-// type Params = Parameters<typeof zip>; // ✅
-
-// // type First = number
-// type First = Params[0]; // 튜플 형식에서는 이처럼 인덱스를 통해 타입을 값처럼 꺼내올 수 있다.
-
-// // 3 - `Parameters`를 만들어보자.
-// function zip(
-//   x: number,
-//   y: string,
-//   z: boolean
-// ): { x: number; y: string; z: boolean } {
-//   return { x, y, z };
+// // 3 - #### 하나하나 살펴보자, -readonly [P in keyof T]
+// interface PromiseConstructor {
+//   all<T extends readonly unknown[] | []>(
+//     values: T
+//   ): Promise<{ -readonly [P in keyof T]: Awaited<T[P]> }>;
 // }
 
-// // // (1) T를 함수로 타입을 제한: T extends (...args: any) => any
-// // type P<T extends (...args: any) => any> = {};
+// // T 가 뭐였을까?
+// // [p1, p2, p3]을 인자로 받는 Promise.all의
+// // 첫 T는 제네릭으로 [p1, p2, p3] 를 받는다.
+// // T는 [p1, p2, p3] 이다.
 
-// // (2) T extends (...args: infer A) => any
-// // 새로운 제네릭 A가 추가됐고, infer라는 키워드가 보인다.
-// type P<T extends (...args: any) => any> = T extends (...args: infer A) => any
-//   ? A
-//   : never;
+// // typeof T는 뭘까?
 
-// // type Params = [x: number, y: string, z: boolean] // ✅
-// type Params = P<typeof zip>;
-// // type First = number // ✅
-// type First = Params[0];
+// // 배열을 타입으로 받으면 다음과 같다.
+// // [타입추론] const arr: readonly [1, 2, 3]
+// const arr = [1, 2, 3] as const;
 
-// // ※ infer (inference): 1. 추론(한 것), (=deduction) 2. 추론 (행위)
-// // infer는 타입스크립트가 알아서 추론하게 하는 문법이다. (extends에서만 사용가능하다.)
-// // 그래서 infer A의 의미는 A의 자리. 즉, 매개변수 자리의 타입을 추론하라는 의미이고 추론 값이 있으면 그 값을 쓰라는 의미이다.
-// // infer를 사용한 다른 사람의 코드는 어느정도 읽기 쉬운 편이지만, 내가 쓰게 되는 경우 infer의 위치를 어디로 둬야 좋을지 헷갈릴 수 있다.
+// // 이 arr을 또 keyof로 추출하면 다음과 같다.
+// // [타입추론] type Arr = keyof readonly [1, 2, 3]
+// type Arr = keyof typeof arr;
 
-// // 4 - 그렇다면 오직 연습 뿐(!). 반환값을 타입으로 하는 유틸리티 타입을 만들어보자.
-// function zip(
-//   x: number,
-//   y: string,
-//   z: boolean
-// ): { x: number; y: string; z: boolean } {
-//   return { x, y, z };
-// }
+// // 검증
+// const key1: Arr = 0;
+// const key2: Arr = 4;
+// const key3: Arr = 999999;
+// const key4: Arr = "length";
+// const key5: Arr = "2";
+// const key6: Arr = "3"; // ❌
 
-// type R<T extends (...args: any) => any> = T extends (...args: any) => infer A
-//   ? A
-//   : never;
+// // 이를 통해 살펴본 내용을 객체로 표현해보자면... { '0': p1, '1': p2, '2': p3, length: 3 }가 되는 듯 해보인다. (키가 문자열)
+// // 'readonly [1, 2, 3]'의 keyof기 때문이다.
 
-// // type Return = { // ✅ - 그렇지만, 튜플의 형태는 아니다 0.0
-// //   x: number;
-// //   y: string;
-// //   z: boolean;
-// // };
-// type Return = R<typeof zip>;
+// // 그러므로,
+// // keyof T는 '0' | '1' | '2' | length 가 될 것이다.
 
-// // 이와 같은 유틸리티 타입은 실제로도 있다.
-// // type Ret = { // ✅
-// //   x: number;
-// //   y: string;
-// //   z: boolean;
-// // }
-// type Ret = ReturnType<typeof zip>;
+// const p1 = Promise.resolve(1)
+//   .then((a) => a + 1)
+//   .then((a) => a + 1)
+//   .then((a) => a.toString());
+// const p2 = Promise.resolve(2);
+// const p3 = new Promise((res, rej) => {
+//   setTimeout(res, 1000);
+// });
 
-// // 이제 어떤 함수든 그 함수의 인자나 리턴의 타입을 자유자재로 뜯어올 수 있다. 😅
+// // 그리고 -readonly 로 인해
+// // 인자로 받을 때는 readonly로 받지만, (readonly [1, 2, 3])
+// Promise.all([p1, p2, p3]).then((result) => {
+//   // result에서는 readonly를 제거해 이를 변경하면서 사용할 수 있다.
+//   console.log(result);
+// });
 
-// 5 - ### `ConstructorParameters`와 `InstanceType`
+// 4 - #### `in` 다시 한번 살펴보기 (Mapped Types)
+// [handbook](https://www.typescriptlang.org/docs/handbook/2/mapped-types.html);
+// [pull request](https://github.com/Microsoft/TypeScript/pull/12114)
 
-// ReturnType과 비슷한 구조를 하고있다.
+// 5 - #### Awaited<T[P]>
+// Awaited는 어떤 마법을 부려서 then체인을 한번에 해석해서 최종타입으로 바꿔줄 수 있었을까?
 
-// /**
-//  * Obtain the parameters of a constructor function type in a tuple
-//  */
-// type ConstructorParameters<T extends abstract new (...args: any) => any> =
-//   T extends abstract new (...args: infer P) => any ? P : never;
-// // ※ abstract new (...args: any) => any 생성자 모양
+// 일단, Awaited 코드를 살펴보기 전에 Awaited<T[P]> 에서 T[P]로 배열의 값들을 전달해주고 있었다는 것을 기억하자.
 
-// /**
-//  * Obtain the return type of a constructor function type
-//  */
-// type InstanceType<T extends abstract new (...args: any) => any> =
-//   T extends abstract new (...args: any) => infer R ? R : any;
+// lib.es5.d.ts
+// 그러면 여기의 <T>는 배열의 값들일 것이다.
+type Awaited<T> = T extends null | undefined
+  ? T // special case for `null | undefined` when not in `--strictNullChecks` mode
+  : T extends object & { then(onfulfilled: infer F): any } // `await` only unwraps object types with a callable `then`. Non-object types are not unwrapped
+  ? F extends (value: infer V, ...args: any) => any // if the argument to `then` is callable, extracts the first argument
+    ? Awaited<V> // recursively unwrap the value
+    : never // the argument to `then` was not callable
+  : T; // non-object or non-thenable
 
-// // 6 - #### `ConstructorParameters`
-// class A {
-//   a: string;
-//   b: number;
-//   c: boolean;
+// 자, 컨디셔널 타입이 3번에 겁먹지 말고 천천히 살펴보자 ^^;
+// 첫번째 라인부터 살펴보면 삼항연산자를 사용해서 null 또는 undefined면 그대로 반환하라는 의미인데, 
+// 그런 상황은 없다고 생각하고 첫번째 라인은 우선 제거해보자.
+type Awaited<T> = T extends object & { then(onfulfilled: infer F): any } // `await` only unwraps object types with a callable `then`. Non-object types are not unwrapped
+  ? F extends (value: infer V, ...args: any) => any // if the argument to `then` is callable, extracts the first argument
+    ? Awaited<V> // recursively unwrap the value
+    : never // the argument to `then` was not callable
+  : T; // non-object or non-thenable
 
-//   constructor(a: string, b: number, c: boolean) {
-//     this.a = a;
-//     this.b = b;
-//     this.c = c;
-//   }
-// }
-// const c = new A("123", 456, false);
+// 그럼 이제 T extends object & { then(onfulfilled: infer F) 가 만족하는지를 살펴보면 되는데,
+// Promise는 객체니까 일단 object는 패스.
+// 그리고 Promise는 then이라는 메서드가 있는 객체인지를 보면 패스.
 
-// // type C = [a: string, b: number, c: boolean] // ✅
-// type C = ConstructorParameters<typeof A>; // typeof 클래스가 생성자 (abstract new (...args: any) => any가 생성자라는 걸 알려주는 부분)
+// F는 then에 들어간 infer로 인해 타입으로 추론이 될 것인데,
+// then의 타입을 살펴보면
+interface P<T> {
+  then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): Promise<TResult1 | TResult2>;
+}
+// 에서 '((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null' 부분이 then의 타입이 될 것이다.
+// 이 부분을 타입스크립트가 infer로 추론을 할 것이다.
 
-// // type I = A
-// type I = InstanceType<typeof A>; // new A("123", 456, false);
 
-// const a: I = A; // ❌
-// const b: I = new A("123", 456, false); // ✅ 인스턴스(new)
+// 그럼 이제 infer로 추론한 F를 살펴보자.
+F extends (value: infer V, ...args: any) => any  ? Awaited<V> : never
+// (value: infer V, ...args: any) => any 를 통해 함수꼴인지는 확인을 했다. (위의 then의 타입이 함수꼴)
+// 그래서 Awatied<V>로 간다. 결과적으로는 V에 인자로 넘긴 value의 타입이 들어오고 재귀로 호출한다.
 
-// // 7 - ### 기타
-// // 대부분의 유틸리티 타입을 살펴봤지만, 내부적으로 구현되어있는 유틸리티 타입들도 여러개가 있다.
+// 그럼 다시 정확히 왜 string이 나오는지를 살펴보자.
+const p1 = Promise.resolve(1)
+  .then((a) => a + 1)
+  .then((a) => a + 1)
+  .then((a) => a.toString());
 
-// // 8 - #### `Lowercase`
-// const a = "Hello World";
-// const b: Lowercase<typeof a> = "Hello World"; // ❌ Type '"Hello World"' is not assignable to type '"hello world"'.ts(2322)
+// (1) .resolve(1)
+// lib.es2015.promise.d.ts
+resolve<T>(value: T | PromiseLike<T>): Promise<T>;
 
-// // 그래서 대략 이런 느낌이지만, toLowerCase의 반환타입이 string이라 대입이 되진 않는다. ^^;
-// const c: Lowercase<typeof a> = a.toLowerCase(); // ❌ Type 'string' is not assignable to type '"hello world"'.ts(2322)
+// resolve의 value가 number이므로 T가 number가 되서, Promise<number>
 
-// // 8 - #### lib.es5.d.ts의 `Lowercase`
+// (2) .then((a) => a + 1)
+then<TResult1 = T, TResult2 = never>(onfulfilled?: ((value: T) => TResult1 | PromiseLike<TResult1>) | undefined | null, onrejected?: ((reason: any) => TResult2 | PromiseLike<TResult2>) | undefined | null): Promise<TResult1 | TResult2>;
 
-// // lib.es5.d.ts
+// 매개변수 타입이 길어 헷갈릴 수 있지만, 마지막 부분의 : Promise<TResult1 | TResult2>; 가 리턴타입이다.
+// TResult2 = never로 사용하지 않으니까 넘어가고, 또 우리는 예외상황을 다루고 있는 것은 아니니까 이를 다루는 인자는 넘어가
+// TResult1에 대한 타입에만 집중하면 된다. 함수꼴로 받으며 value로 받은 타입 T를 TResult1의 타입(T)으로 사용하고 있다는 걸 알 수 있으므로,
+// .then((a) => a + 1)가 onfulfilled된 인자 value의 T는 number 이므로, Promise<number> 가 된다.
 
-// /**
-//  * Convert string literal type to lowercase
-//  */
-// type Lowercase<S extends string> = intrinsic;
-// // 구현이 안 되어 있다.
+// (3) .then((a) => a + 1) 도 마찬가지로 Promise<number>
 
-// // 9 - #### `ThisType`
-// /**
-//  * Marker for contextual 'this' type
-//  */
-// interface ThisType<T> {}
+// (4) 그리고 마지막으로 .then((a) => a.toString()) 이 string으로 바뀌었으니까 Promise<number>
 
-// 제로초 readme
+// ❌
+// 흐름을 대략적으로 적어보자면,
+// null, undefined 검사는 넘어가고
+// 들어온 타입이 onfulfilled인자를 갖는 then메서드를 가지고 있는 타입(Promise 타입)이면, 이 인자를 추론하고 (F)
+// 그 value. 즉, then의 인자의 타입을 추론해 재귀로 Awaited가 돌면,
+// 더이상 thenable한 타입이 아니므로 그 타입을 돌려준다.
+
+// #### Promise와 모양만 같다면 인자자리의 타입을 잘 추론해낸다.
+// [타입추론] type Result = number
+type Result = Awaited<{ then(onfulfilled: (v: number) => number): any }>
+
+// 설명을 들으니까야 흐름은 어느정도 보이지만...
+// 만들어야 하는 상황이라면...?...
+
+// Promise.then의 흐름에 대해서도 학습이 되는 것 같다.
+
+// YOU DON`T KNOW JS - 238page
+// duck typing
+// thenable
